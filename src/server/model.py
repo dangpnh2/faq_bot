c5af071d73bfd927c72b355520c4d8348c9fcf10
thanhphuong163 <thanhphuong.its@gmail.com>
2024-02-27 22:15:05 -0700

    updated threshold and added returned score in < threshold case

--- src/server/model.py
+++ src/server/model.py
@@ -56,7 +56,7 @@ class FAQBot:
         calc_score = torch.nn.CosineSimilarity(dim=1, eps=1e-08)
         dists = calc_score(user_qe, self.q_embeddings)
         ans_idx = torch.argmax(dists).cpu().numpy()
-        best_score = dists[ans_idx]
+        best_score = dists[ans_idx].item()
         result = {}
         if best_score >= self.threshold:
             result['Score'] = best_score
@@ -64,6 +64,7 @@ class FAQBot:
             result['Answer'] = self.database['a'][ans_idx]
         else:
             ans = self.general_model(question, do_sample=True, min_length=10, max_new_tokens=100)[0]
+            result['Score'] = best_score
             result['Answer'] = ans['generated_text'].split('\n\n')[1]
         return result
 
@@ -73,11 +74,15 @@ def load_all_models():
 
     qa_model = SentenceTransformer('sentence-transformers/multi-qa-MiniLM-L6-cos-v1')
     general_model = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B', device=DEVICE)
+
     data = load_data()
+    data['q'] = data['q'].apply(remove_number_start)
+
     faq_bot = FAQBot(
         qa_model=qa_model,
         general_model=general_model,
         database=data,
+        threshold=0.6,
     )
     return faq_bot
 
